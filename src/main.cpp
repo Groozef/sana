2e1f72df40489c4da7b1876f5cca04af113d21ab
Temirlan <groozeff@gmail.com>
2026-05-02 13:09:23 +0500

    configured cmake.txt . wrote the code to open the window. Laying the foundation for the project

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
+#define GLFW_INCLUDE_NONE
+
 #include <iostream>
-int main() {
-    std::cout << "Sana v0.1.0\n";
+#include <GLFW/glfw3.h>
+
+int main()
+{
+    try
+    {
+        if (!glfwInit())
+        {
+            throw std::runtime_error("Failed to initialize GLFW");
+        }
+        GLFWwindow *window = glfwCreateWindow(1280, 720, "SANA v0.0.1", nullptr, nullptr);
+
+        if (!window)
+        {
+            throw std::runtime_error("Failed to create Window");
+        }
+        glfwMakeContextCurrent(window);
+
+
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        glfwDestroyWindow(window);
+        glfwTerminate();
+    }
+    catch (const std::exception &err)
+    {
+        std::cerr << "Error: " << err.what() << std::endl;
+        glfwTerminate();
+        return -1;
+    }
     return 0;
-};
+}
